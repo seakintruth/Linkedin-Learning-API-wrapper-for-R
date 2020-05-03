72af687b9c335d2e3f8d6705e3cfc5142adff101
Jeremy D. Gerdes <seakintruth@gmail.com>
2020-05-03 11:11:31 -0400

    Update getLinkedinLearningApiToken.R
    added handleProxy helper function still working on these functions

--- R/getLinkedinLearningApiToken.R
+++ R/getLinkedinLearningApiToken.R
@@ -1,4 +1,4 @@
-pacman::p_load(httr,jsonlite,getPass,tcltk,curl)
+pacman::p_load(httr,jsonlite,getPass,tcltk,curl,RCurl,stringr)
 
 # --- get.linkedin.access.token ---
 # Linked in learning API keys are valid for several months at a time, 
@@ -20,57 +20,11 @@ get.linkedin.access.token <- function(
 	apiClientIdSecretPath="",
 	apiKeyRdsPath=""
 ){
-    .configureForProxy <- function(){
-	currentProxyInfo <- curl::ie_proxy_info()
-	httpTestUrl <- "http://httpbin.org/get"
-	httpsTestUrl <- "https://httpbin.org/get"
-	if(!is.null(currentProxyInfo$Proxy)){
-		# a function to work behind a VPN...
-		# See: https://cran.r-project.org/web/packages/curl/vignettes/windows.html
-		currentProxyInfo <- curl::ie_proxy_info()
-		ieProxies <- strsplit(currentProxyInfo$Proxy,";")
-      		# find the list element that contains https or http 
-		#[ASSUMPTION] currently only works if each http and https only has one value
-		httpsElementNumber <- unlist(lapply(ieProxies, function(ch) grep("https=", ch)))
-		httpElementNumber <- unlist(lapply(ieProxies, function(ch) grep("http=", ch)))
-		httpProxy <- unlist(ieProxies)[httpElementNumber]
-		httpsProxy <- unlist(ieProxies)[httpsElementNumber]
-		httpProxy <- str_replace(httpProxy,"=","://")
-		httpsProxy <- str_replace(httpsProxy,"=","://")
-		#[TODO] if needed use setx to set windows user environment variables?
-		Sys.setenv(http_proxy = httpProxy)
-		Sys.setenv(https_proxy = httpsProxy)
-		#Sys.setenv(ALL_PROXY = httpsProxy)
-		Sys.setenv(NO_PROXY = currentProxyInfo$ProxyBypass)
-		# this is our test that curl is working
-		#[TODO] neet to handle errors durring requests
-		h <- new_handle(proxy = httpProxy, verbose = TRUE)
-		req <- curl_fetch_memory(httpTestUrl, handle = h)
-		# Can't curl a https site, as it returns this error:
-		#Error in curl_fetch_memory("https://httpbin.org/get", handle = h) : 
-		#  Unsupported proxy 'https://NMCIProxyB1Secure:8443', libcurl is built without the HTTPS-proxy support.
-		#h <- new_handle(proxy = httpsProxy, verbose = TRUE)
-		#req <- curl_fetch_memory("https://httpbin.org/get", handle = h)
-			# More Tests
-		httr::set_config(config(ssl_verifypeer = 0L, ssl_verifyhost = 0L))	
-		httr::set_config(config(ssl_options = 1L))
-            httr::set_config(use_proxy(url=httpProxy,port=8080, username=Sys.getenv("username")))
-		request <- httr::GET(httpTestUrl)
-	} else {
-		Sys.setenv(ALL_PROXY = "")
-		Sys.setenv(http_proxy = "")
-		Sys.setenv(https_proxy = "")
-		Sys.setenv(NO_PROXY = "")
-		# Test that curl works
-		request <- httr::GET(httpTestUrl)
-	}
-	return(request$status_code == 200) # && req$status_code == 200 && 
-  }
   .getApiKeyIfValid <- function(apiKeyRdsPath){
 	if(
 		file.exists(apiKeyRdsPath) && 
 		tolower(tools::file_ext(apiKeyRdsPath)) == "rds"
-	){
+ 	){
 		token <- readRDS(apiKeyRdsPath)
 		#Determin if the token has expired or will within a day
 		if (
@@ -195,8 +149,11 @@ get.linkedin.access.token <- function(
 		# ---
 		# using httr::POST, alternativly we could use ?RCurl::postForm
 		# may or may not need to use set_config
+		# [TODO] Currently can't get https over proxy working
 		# handle proxy if needed
-		.configureForProxy()
+		https_method <- handleProxy()
+
+		# [TODO] use a working https_method to get API Key, and API Queries
 
 		# build form data
 		oauth_params <-  as.list(c(
@@ -247,4 +204,144 @@ get.linkedin.access.token <- function(
 	# we should delete the apiToken file and call this function again
 	return(unname(potentialToken["accessToken"]))
   }
+}
+
+handleProxy <- function(){
+	currentProxyInfo <- curl::ie_proxy_info()
+	httpTestUrl <- "http://httpbin.org/get"
+	httpsTestUrl <- "https://httpbin.org/get"
+	if(!is.null(currentProxyInfo$Proxy)){
+		useProxy <- TRUE
+
+		#[TODO] if needed use setx to set windows user environment variables?
+		Sys.setenv(http_proxy = httpProxy)
+		Sys.setenv(https_proxy = httpsProxy)
+		Sys.setenv(ALL_PROXY = httpProxyAll )
+		Sys.setenv(NO_PROXY = currentProxyInfo$ProxyBypass)
+		Sys.setenv(CURL_SSL_BACKEND="openssl")
+
+		# a function to work behind a VPN...
+		# See: https://cran.r-project.org/web/packages/curl/vignettes/windows.html
+		ieProxies <- strsplit(currentProxyInfo$Proxy,";")
+      		# find the list element that contains https or http 
+		#[ASSUMPTION] currently only works if each http and https only has one value
+		httpsElementNumber <- unlist(lapply(ieProxies, function(ch) grep("https=", ch)))
+		httpElementNumber <- unlist(lapply(ieProxies, function(ch) grep("http=", ch)))
+		httpProxy <- unlist(ieProxies)[httpElementNumber]
+		httpsProxy <- unlist(ieProxies)[httpsElementNumber]
+		httpProxyAll <- unlist(ieProxies)
+		httpProxy <- stringr::str_replace(httpProxy,"=","://")
+		httpsProxy <- stringr::str_replace(httpsProxy,"=","://")
+		httpProxyAll <- stringr::str_replace(httpProxyAll,"=","://")			
+		httpProxyAll <- paste(httpProxyAll ,collapse=";")
+		
+		#some options that might be helpful
+		#[TODO] neet to handle errors durring requests
+		#httr::set_config(config(ssl_verifypeer = 0L, ssl_verifyhost = 0L))	
+		#httr::set_config(config(ssl_options = 1L))
+            #httr::set_config(use_proxy(url=httpProxy,port=8080, username=Sys.getenv("username")))
+
+		# set default values
+		httpCurlSuccess <- FALSE
+		httpsCurlSuccess <- FALSE
+		httpHttrSuccess <- FALSE
+		httpsHttrSuccess <- FALSE
+		httpBaseUrlSuccess <- FALSE
+		httpsBaseUrlSuccess <- FALSE
+		
+		
+		#libcurl documentation: https://ec.haxx.se/libcurl/libcurl-proxies
+		h <- curl::new_handle(proxy = httpProxy, verbose = TRUE)
+
+		# check named vector value with: proxyAccess["httpsHttrSuccess"]
+
+		# Can't curl a https site, as it returns this error:
+		#Error in curl_fetch_memory("https://httpbin.org/get", handle = h) : 
+		#  Unsupported proxy '...', libcurl is built without the HTTPS-proxy support.
+		#h <- new_handle(proxy = httpsProxy, verbose = TRUE)
+		#req <- curl_fetch_memory("https://httpbin.org/get", handle = h)
+		# More Tests
+		# httr::GET(httpTestUrl) works but not for https ?
+
+		
+	} else {
+		useProxy <- FALSE
+		h <- curl::new_handle(verbose = TRUE)
+	}
+	# Thes are tests that internet access over http and https are successfull with different methods
+	curlHttpTest = tryCatch({
+		req <- curl::curl_fetch_memory(httpTestUrl, handle = h)
+		httpCurlSuccess <- req$status_code == 200
+	}, warning = function(warr) {
+	    	message(paste0("Warning: http curl_fetch_memory test;",warr))
+	}, error = function(err) {
+	    	message(paste0("Error: http curl_fetch_memory test;",err))
+	}, finally = {
+	    	#message("Attempted http curl_fetch_memory")
+	})
+	
+	curlHttpsTest = tryCatch({
+		req_s <- curl::curl_fetch_memory(httpTestUrl, handle = h)
+		httpsCurlSuccess <- req_s$status_code == 200
+	}, warning = function(warr) {
+	    	message(paste0("Warning: https curl_fetch_memory test;",warr))
+	}, error = function(err) {
+	    	message(paste0("Error: https curl_fetch_memory test;",err))
+	}, finally = {
+	    	#message("Attempted https curl_fetch_memory")
+	})
+
+	httrHttpTest = tryCatch({
+		request <- httr::GET(httpTestUrl)
+		httpHttrSuccess <- request$status_code == 200
+	}, warning = function(warr) {
+	    	message(paste0("Warning: http httr::GET test;",warr))
+	}, error = function(err) {
+	    	message(paste0("Error: http httr::GET test;",err))
+	}, finally = {
+	    	#message("Attempted http httr::GET")
+	})
+
+	httrHttpsTest = tryCatch({
+		request_s <- httr::GET(httpsTestUrl)
+		httpsHttrSuccess <- request_s$status_code == 200
+	}, warning = function(warr) {
+	    	message(paste0("Warning: https httr::GET test;",warr))
+	}, error = function(err) {
+	    	message(paste0("Error: https httr::GET test;",err))
+	}, finally = {
+	    	#message("Attempted https httr::GET")
+	})
+
+	baseUrlHttpTest = tryCatch({
+		httpBaseUrlSuccess <- readLines(base::url(httpTestUrl))[2] == "  \"args\": {}, "
+	}, warning = function(warr) {
+	    	message(paste0("Warning: http base::url test;",warr))
+	}, error = function(err) {
+	    	message(paste0("Error: http base::url test;",err))
+	}, finally = {
+	    	#message("Attempted http base::url")
+	})
+
+	baseUrlHttpsTest = tryCatch({
+		httpsBaseUrlSuccess <- readLines(base::url(httpsTestUrl))[2] == "  \"args\": {}, "
+	}, warning = function(warr) {
+	    	message(paste0("Warning: https base::url test;",warr))
+	}, error = function(err) {
+	    	message(paste0("Error: https base::url test;",err))
+	}, finally = {
+	    	#message("Attempted https base::url")
+	})
+
+	#Return a named vector of successes/failures
+	proxyAccess <- c(
+		useProxy=useProxy,
+		httpCurlSuccess=httpCurlSuccess,
+		httpsCurlSuccess=httpsCurlSuccess,
+		httpHttrSuccess=httpHttrSuccess, 
+		httpsHttrSuccess=httpsHttrSuccess,
+		httpBaseUrlSuccess=httpBaseUrlSuccess, 
+		httpsBaseUrlSuccess=httpsBaseUrlSuccess 
+	)
+	return(proxyAccess)
 }
